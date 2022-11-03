d46259aff7e3ff07133d73c453ca74e16e73eeb1
g00s3_m4n <petrovih384@gmail.com>
2022-11-03 16:41:09 +0700

    slider

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        loop: true,
+        navText: ["<img src='assets/images/icons/arrow.svg'>","<img src='assets/images/icons/arrow.svg'>"]
+    });
+});
