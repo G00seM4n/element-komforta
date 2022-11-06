d182ee04617623722aaaa2aa845fae7303ed5071
g00s3_m4n <petrovih384@gmail.com>
2022-11-06 19:02:19 +0700

    main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,25 @@
+/* Слайдер */
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         items: 1,
         nav: true,
         loop: true,
-        navText: ["<img src='assets/images/icons/arrow.svg'>","<img src='assets/images/icons/arrow.svg'>"]
+        navContainer: ".slider-navigation",
+        dotsContainer: ".slider-dots",
+        navText: ["<img class='left' src='assets/images/icons/arrow.svg'>","<img class='right' src='assets/images/icons/arrow.svg'>"]
+    });
+    
+    let accordions = $('.accordion-collapse')
+    $.each(accordions, function(i, el){
+        $(el).on('hidden.bs.collapse', function () {
+            $(this).parent().removeClass('active')
+        })
+        $(el).on('show.bs.collapse', function () {
+            $(this).parent().addClass('active')
+        })
     });
 });
+
+
+
+/* модальные окна */
