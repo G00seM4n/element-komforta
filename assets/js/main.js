6b5654640bf3e31abf0030257bc92021ad777364
g00s3_m4n <petrovih384@gmail.com>
2022-11-11 15:10:08 +0700

    правки

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,23 @@
 $(document).ready(function () {
     /* Слайдер */
-    $(".owl-carousel").owlCarousel({
+    $(".slider .owl-carousel").owlCarousel({
         items: 1,
         nav: true,
-        loop: true,
+        navContainer: ".slider-navigation",
+        dotsContainer: ".slider-dots",
+        navText: ["<img class='left' src='assets/images/icons/arrow.svg'>", "<img class='right' src='assets/images/icons/arrow.svg'>"]
+    });
+    
+    $(".modal-full .owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
         navContainer: ".slider-navigation",
         dotsContainer: ".slider-dots",
         navText: ["<img class='left' src='assets/images/icons/arrow.svg'>", "<img class='right' src='assets/images/icons/arrow.svg'>"]
     });
 
     /* Аккордион */
-    let accordions = $('.accordion-collapse');
+    let accordions = $('.accordion-collapse+active');
 
     $.each(accordions, function (i, el) {
         $(el).on('hidden.bs.collapse', () => {
@@ -39,5 +46,22 @@ $(document).ready(function () {
             menuAddressBtn.text($(el).text());
             menuAddressBtn.append(`<svg class="arrow" width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 0.5L7 6L12.5 0.5L14 1L7 8L0 1L1.5 0.5Z" fill="#383606"/></svg>`)
         });
+    });
+    
+    /* Полноэкранное модальное окно */
+    let modalFull = $('.modal-full');
+    let modalFullLink = $('.modal-full-link');
+    let modalFullClose = $('.modal-full .btn-close');
+
+    modalFullLink.click(function () {
+        modalFull.addClass('active');
+        modalFullLink.addClass('active');
+        document.body.style.overflow = 'hidden';
+    });
+
+    modalFullClose.click(function () {
+        modalFull.removeClass('active');
+        modalFullLink.removeClass('active');
+        document.body.style.overflow = 'unset';
     });
 });
