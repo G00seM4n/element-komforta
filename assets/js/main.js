cffdc2fd191113da4575a1ce7f982ea21c716ba5
g00s3_m4n <petrovih384@gmail.com>
2022-11-15 19:03:18 +0700

    adaptive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,65 @@
 $(document).ready(function () {
-    /* Слайдер */
-    $(".slider .owl-carousel").owlCarousel({
-        items: 1,
-        nav: true,
-        navContainer: ".slider-navigation",
-        dotsContainer: ".slider-dots",
-        navText: ["<img class='left' src='assets/images/icons/arrow.svg'>", "<img class='right' src='assets/images/icons/arrow.svg'>"]
+    /* Отступ сверху */
+    let headerHeight = $('header').height();
+    $('main').css('margin-top',headerHeight);
+
+    /* Меню */
+    $('.burger-nav').click(function(){
+        $('.mobile-menu').toggleClass('active');
     });
     
-    $(".modal-full .owl-carousel").owlCarousel({
+
+    /* Слайдер 1 */
+    $("#slider1").owlCarousel({
         items: 1,
-        nav: true,
-        navContainer: ".slider-navigation",
-        dotsContainer: ".slider-dots",
-        navText: ["<img class='left' src='assets/images/icons/arrow.svg'>", "<img class='right' src='assets/images/icons/arrow.svg'>"]
+        navContainer: ".slider1 .slider-navigation",
+        dotsContainer: ".slider1 .slider-dots",
+        navText: ["<img class='left' src='assets/images/icons/arrow.svg'>", "<img class='right' src='assets/images/icons/arrow.svg'>"],
+    });
+
+    /* Слайдер 2 */
+    $("#slider2").owlCarousel({
+        items: 1,
+        
+        dots: false,
+        navText: ["<img class='left' src='assets/images/icons/arrow.svg'>", "<img class='right' src='assets/images/icons/arrow.svg'>"],
+        URLhashListener: 'URLHash',
+        responsive : {
+            0: {
+                navContainer: false,
+                nav: false
+            },
+            768 : {
+                navContainer: ".slider2 .slider-navigation",
+            }
+        }
+    });
+
+    slides = $("#slider2 .owl-item");
+    let navImg = $('.slider-navigation-imgs>a');
+
+    window.addEventListener('hashchange', () => {
+        let activeSlide = $('#slider2 .owl-item.active').children().data().hash;
+        
+        $.each(navImg, function (i, el) {
+            if(window.location.hash.length != 0){
+                $(el).removeClass('active');
+            }
+            if(activeSlide === el.hash.replace('#','')){
+                $(el).addClass('active');
+            }
+        });
     });
 
     /* Аккордион */
-    let accordions = $('.accordion-collapse+active');
+    let accordions = $('.accordion-collapse');
 
     $.each(accordions, function (i, el) {
-        $(el).on('hidden.bs.collapse', () => {
-            $(this).parent().removeClass('active');
-        });
         $(el).on('show.bs.collapse', () => {
             $(this).parent().addClass('active');
+        });
+        $(el).on('hidden.bs.collapse', () => {
+            $(this).parent().removeClass('active');
         });
     });
 
@@ -42,12 +77,11 @@ $(document).ready(function () {
         $(el).click(function () {
             list.removeClass('active');
             menuAddressBtn.removeClass('active');
-
             menuAddressBtn.text($(el).text());
             menuAddressBtn.append(`<svg class="arrow" width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 0.5L7 6L12.5 0.5L14 1L7 8L0 1L1.5 0.5Z" fill="#383606"/></svg>`)
         });
     });
-    
+
     /* Полноэкранное модальное окно */
     let modalFull = $('.modal-full');
     let modalFullLink = $('.modal-full-link');
